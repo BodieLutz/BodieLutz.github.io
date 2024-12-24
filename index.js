664bcb2b6812f4af735e03cbf3f9e0e5a3c592e7
Bodie Lutz <bodie5558@gmail.com>
2024-12-24 15:30:56 -0700

    Finished styling on nav. HTML and CSS boilerplate. JavaScript to load content dyanmically

--- index.js
+++ index.js
@@ -13,6 +13,8 @@
 document.addEventListener("DOMContentLoaded", () => {
     const links = document.querySelectorAll(".nav-link");
     const nav = document.querySelector(".vertical-nav");
+    const content_frame = document.getElementById("content-frame");
+
   
     links.forEach((link, index) => {
       link.addEventListener("click", (e) => {
@@ -20,9 +22,12 @@ document.addEventListener("DOMContentLoaded", () => {
   
         // Remove active class from all links
         links.forEach((l) => l.classList.remove("active"));
-  
         // Add active class to the clicked link
         link.classList.add("active");
+
+        //Get HTML file from link
+        const href = link.getAttribute("href");
+        content_frame.setAttribute("src", href);
       });
     });
   });
