1cac2c6de6b09044bd59de5a240899a8bc64e306
Bodie Lutz <bodie5558@gmail.com>
2024-12-26 14:34:56 -0700

    make resume button 'navigate' to resume page

--- javascript/about.js
+++ javascript/about.js
@@ -3,12 +3,24 @@ document.addEventListener("DOMContentLoaded", () => {
     const parentDocument = window.parent.document;
     const links = parentDocument.querySelectorAll(".nav-link");
     const resume_link = parentDocument.getElementById("resume-link");
+    const content_frame = parentDocument.getElementById("content-frame");
   
     //add event listener to resume button
     button.addEventListener("click", (e) => {
         e.preventDefault();
-        alert("Button Works!");
-        alert(links);
-        alert(resume_link);
+
+        links.forEach((link) => {
+            //remove active class from all links
+            link.classList.remove("active");  
+          });
+        
+        //add active class to resume link
+        resume_link.classList.add("active");
+        
+        //get resume href
+        const href = resume_link.getAttribute("href");
+
+        //load html of resume
+        content_frame.setAttribute("src", href);
     });
   });
