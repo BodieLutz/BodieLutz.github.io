6c8533a5e1ffc24ec5bc0bc2f5d70e606ed8723e
Bodie Lutz <bodie5558@gmail.com>
2024-12-26 14:09:20 -0700

    alert links of parent document for testing

--- javascript/about.js
+++ javascript/about.js
@@ -2,10 +2,13 @@ document.addEventListener("DOMContentLoaded", () => {
     const button = document.getElementById("resume-btn");
     const parentDocument = window.parent.document;
     const links = parentDocument.querySelectorAll(".nav-link");
+    const resume_link = parentDocument.getElementById("resume-link");
   
     //add event listener to resume button
     button.addEventListener("click", (e) => {
         e.preventDefault();
         alert("Button Works!");
+        alert(links);
+        alert(resume_link);
     });
   });
