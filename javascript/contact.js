b48f26aece30a1b3b14b7383e799225ea0d96474
Bodie Lutz <bodie5558@gmail.com>
2024-12-29 21:26:29 -0700

    ripple on email button

--- javascript/contact.js
+++ javascript/contact.js
@@ -4,7 +4,6 @@ document.addEventListener("DOMContentLoaded", () => {
     const contactLink = parentDocument.getElementById("contact-link");
 
     if (contactLink.classList.contains("active")) {
-        alert("Contact link is active");
         // emailButton.style.animation = "ripple 1s linear 0s infinite;";
         // Define the keyframes for the ripple effect
         const rippleKeyframes = [
@@ -20,7 +19,7 @@ document.addEventListener("DOMContentLoaded", () => {
             easing: 'linear', // Easing function
         };
 
-        contactLink.animate(rippleKeyframes, rippleOptions);
+        emailButton.animate(rippleKeyframes, rippleOptions);
     }
 
 });
