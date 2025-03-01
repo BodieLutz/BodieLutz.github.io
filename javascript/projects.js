9b209f3259073dfd71ab707185ab33d136e6481c
Bodie Lutz <bodie5558@gmail.com>
2025-03-01 14:56:32 -0700

    add styling and link JS to HTML

--- javascript/projects.js
+++ javascript/projects.js
@@ -18,30 +18,37 @@ document.addEventListener("DOMContentLoaded", () => {
 
     //Javascript generator to generate HTML for projects
     function generateProjects(projects) {
-        const timeline = document.getElementById("timeline");
+        const timeline = document.getElementById("projects-timeline");
         //generate HTML for each project
         projects.forEach((project) => {
+            //if project must be private...
             if (project.link === "#") {
                 const projectCard = `
-                <div class="card mb-3">
-                    <div class="cardBody">
-                        <h5 class="card-title">{project.title}</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">{new Date(project.date).toLocaleDateString()}</h6>
-                        <p class="card-text">{project.description}</p>
-                        <p class="non-link">Sorry, this was a school project and must remain private :)</p>
+                <div class="timeline-item">
+                    <div class="card mb-3">
+                        <div class="cardBody">
+                            <h5 class="card-title">{project.title}</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">{new Date(project.date).toLocaleDateString()}</h6>
+                            <p class="card-text">{project.description}</p>
+                            <p class="non-link">Sorry, this was a school project and must remain private :)</p>
+                        </div>
                     </div>
                 </div>`; 
-            } else {
+            } 
+            
+            else {
                 const projectCard = `
-                <div class="card mb-3">
-                    <div class="cardBody">
-                        <h5 class="card-title">{project.title}</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">{new Date(project.date).toLocaleDateString()}</h6>
-                        <p class="card-text">{project.description}</p>
-                        <a href="{project.link}" class="card-link">View Project</a>
+                <div class="timeline-item">
+                    <div class="card mb-3">
+                        <div class="cardBody">
+                            <h5 class="card-title">{project.title}</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">{new Date(project.date).toLocaleDateString()}</h6>
+                            <p class="card-text">{project.description}</p>
+                            <a href="{project.link}" class="card-link">View Project</a>
+                        </div>
                     </div>
                 </div>`; 
-        }
+            }
             timeline.innerHTML += projectCard;         
         });
     }
